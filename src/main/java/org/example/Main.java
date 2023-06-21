0697946cbcbde363f69b13d9e32ded7e9d2f3549
Шиленков вадим <vadimshilenkovlive@yandex.ru>
2023-06-21 17:40:03 +0300

    обьединение
    5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Изменение номер один");
+        System.out.println("Изменение номер два");
+        System.out.println("Изменение номер 3 должно остаться в 2 коммите");
+        System.out.println("Изменение номер 4 НЕ должно остаться в 2 коммите");
+        System.out.println("34");
+        System.out.println("342523");
+
     }
 }
