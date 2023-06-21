c1fb216a8c5e14d2817c4a9b19f0c835178df12a
Шиленков вадим <vadimshilenkovlive@yandex.ru>
2023-06-21 17:38:11 +0300

    Изменение номер 4 НЕ должно остаться в 2 коммите

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Изменение номер один");
+        System.out.println("Изменение номер два");
+        System.out.println("Изменение номер 3 должно остаться в 2 коммите");
     }
 }
